a7e133fc8b9790d29f71de2fd52d8310b45944de
bestdaniil <d11a30d1172@gmail.com>
2022-02-12 11:42:56 +0300

    Добавил имя в README.md

---
+++ 2Ex.js
@@ -0,0 +1,18 @@
+let m = "yessir";
+
+console.log(typeof m, m);
+
+let n = 92;
+
+console.log(typeof n, n);
+
+let b = true;
+
+console.log(typeof b, b);
+
+let u = kiiil;
+
+console.log(typeof u, u)
+
+let s = 
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # programming-lessons
-123123123
+# Даниил Бестужев 
