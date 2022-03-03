73b130b68e6404a6883ecf9b6a5f22c06112effd
bestdaniil <d11a30d1172@gmail.com>
2022-03-03 10:03:45 +0300

    создаем сервер

--- vebsserver.js
+++ vebsserver.js
@@ -5,6 +5,6 @@ const requestHandler = (request, response) => {
     console.log("User-Agent: " + request.headers["user-agent"]);
     console.log("Все заголовки");
     console.log(request.headers);
-    response.end("work");
+    response.end("worked");
 };
 http.createServer(requestHandler).listen(3000);
