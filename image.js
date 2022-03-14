5f82b7314d283662c1387dea9f3ad4c95cd8555e
bestdaniil <d11a30d1172@gmail.com>
2022-03-14 11:36:27 +0300

    запрос на фото

--- image.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const http = require ("http");
-const requestHandler = (request, response) => {
-    response.setHeader ("Content-Type", "text/html;charset=ytf-8;")
-    if ( request.url ==="/home" || request.url ==="/") {
-        
-    }
-}
