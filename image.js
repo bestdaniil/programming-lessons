2c1e741c3fcedc11348b409973c57795c4db74cf
bestdaniil <d11a30d1172@gmail.com>
2022-03-14 10:18:04 +0300

    отдает клиенту картинку с номером

---
+++ image.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const http = require ("http");
+const requestHandler = (request, response) => {
+    response.setHeader ("Content-Type", "text/html;charset=ytf-8;")
+    if ( request.url ==="/home" || request.url ==="/") {
+        
+    }
+}
