463d74aa736a2e838eba525ca434ece8fa8ab21c
bestdaniil <d11a30d1172@gmail.com>
2022-04-14 16:58:51 +0300

    2 задание 14.04.22

---
+++ cat/cats.js
@@ -0,0 +1,276 @@
+const axios = require('axios');
+const fs = require("fs");
+axios.get('https://http.cat/100', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/101', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat101to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/102', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat102to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/200', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat200to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/201', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat201to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/202', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat202to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/203', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat203to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/204', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat204to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/206', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat206to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/207', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat207to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/300', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat300to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/301', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat301to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/302', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat302to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/303', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat303to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/304', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat304to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/305', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat305to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/307', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat307to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/308', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat308to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/423', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat423to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/424', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat424to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/425', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat425to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/426', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat426to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/429', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat429to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/431', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat431to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/444', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat444to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/450', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat450to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/451', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat451to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/497', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat497to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/498', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat498to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/499', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat499to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/500', {responseType: 'arraybuffer'})
+.then(function (response)
+{
+fs.writeFileSync("cat500to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/501', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat501to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/502', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat502to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/503', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat503to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/504', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat504to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/506', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat506to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/507', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat507to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/508', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat508to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/509', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat509to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/510', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat510to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/511', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat511to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/521', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat521to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/523', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat523to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/525', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat525to.jpg", response.data)
+console.log(response);
+})
+axios.get('https://http.cat/599', {responseType: 'arraybuffer'})
+.then(function (response) 
+{
+fs.writeFileSync("cat599to.jpg", response.data)
+console.log(response);
+})
+.catch(function (error) {
+console.log(error);
+})
+.then(function () {});
